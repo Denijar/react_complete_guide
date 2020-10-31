715cb0e263be79300aa2464d38ff2960a8cf4f44
Denijar <denijar@gmail.com>
2020-11-01 12:42:56 +1300

    added properties to the Person component that can be set from outside of the component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="28" />
+        <Person name="Denise" age="21">My Hobbies: Swimming</Person>
+        <Person name="Henry" age="26"/>
       </div>
     );
     
