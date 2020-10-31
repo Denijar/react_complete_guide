ca9f211b5a99655f68b95a1e29368e7cd4d70c24
Denijar <denijar@gmail.com>
2020-11-01 12:05:00 +1300

    write the equivalent code to the JSX as React.createElement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+    
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+    // this is equivalent to the JSX that is written above
   }
 }
 
