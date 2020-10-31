90ef80e9211f08b31cc0361f7b3300f0e9b804f6
Denijar <denijar@gmail.com>
2020-11-01 12:54:44 +1300

    added a state property in the app component with an array of person objects with names and ages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Denise', age: 21 },
+      { name: 'Henry', age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28" />
-        <Person name="Denise" age="21">My Hobbies: Swimming</Person>
-        <Person name="Henry" age="26"/>
+        <button>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Swimming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     
